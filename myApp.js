8e67205800310e0e24af9567f627df56e826ea0c
abdurrachman <manharist@gmail.com>
2022-04-12 07:19:29 +0000

    Basic Node and Express 04

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 var path = __dirname + "/views/index.html";
+var public = __dirname + "/public";
+
+app.use("/public",express.static(public));
 
 app.get('/', (req, res) => {
   res.sendFile(path);
-})
+});
 
 
 
